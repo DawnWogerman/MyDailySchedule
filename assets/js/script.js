32cfb0bb5f0d49a6cdb671df27b6e33ecaa67bdd
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-04-15 17:42:51 -0600

    restarting js - creating new feature branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let now = moment().format("dddd, MMM Do YYYY");
 let hourNow = parseInt(moment().format("h"));
 let time = moment().format("h:mm a");
+let hourTask = $("textarea");
 
 //Display current date/time in header
 $("#currentDay").text(now);
@@ -30,18 +31,4 @@ $(".currentTime").text(time);
 
 
 
-
-let hourArr = [9,10,11,12,1,2,3,4,5];
-
-if(hourArr[0]>9){
-    console.log("its True");
-} else {
-    console.log("it's false");
-};
-
-console.log(hourArr, typeof hourArr);
-
-
-console.log(now);
-console.log(typeof hourNow , hourNow);
 
