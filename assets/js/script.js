c61e0632d5bad312ff0e57b87a82f8a77626fcca
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-04-16 13:29:19 -0600

    second try

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
 let now = moment().format("dddd, MMM Do YYYY");
 let hourNow = parseInt(moment().format("h"));
 let time = moment().format("h:mm a");
-let hourTask = $("textarea");
+
 
 //Display current date/time in header
 $("#currentDay").text(now);
 $(".currentTime").text(time);
-
-
 
 
 
