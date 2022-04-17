e639bae8cd41c9f4d357c3a154e5c6370b831ecb
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-04-16 19:12:32 -0600

    saving progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let now = moment().format("dddd, MMM Do YYYY");
-let hourNow = parseInt(moment().format("h"));
+let hourNow = moment().format("ha");
 let time = moment().format("h:mm a");
 
 
@@ -8,11 +8,35 @@ $("#currentDay").text(now);
 $(".currentTime").text(time);
 
 
+console.log(hourNow);
 
 
+//match id's from textarea/task fields to current time and set classes
+$(".hourdiv").each(function(){
+    let hourDiv = $(this).attr("id");
+   // console.log(hourDiv);
+   //console.log("hournow" + hourNow);
 
+   if(hourNow == hourDiv){
+       $(this).addClass("present");
+       
+   }
+   else if(hourNow < hourDiv){
+       $(this).removeClass("present");
+       $(this).addClass("future");
+   }
+   else if(hourNow > hourDiv){
+       $(this).removeClass("future");
+       $(this).addClass("past");
+   }
+});
 
-
+//save tasks and set to local storage
+$(".saveBtn").click(function(event){
+    event.preventDefault();
+    let task = $(this).
+    console.log(task);
+})
 
 
 
